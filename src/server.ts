ec976b08b03201c547fae77ab77941b113571b32
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-25 14:54:06 +0100

    variable and date

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
-import express from "express"
-
-import routes from './routes'
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ m: 'hi' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');

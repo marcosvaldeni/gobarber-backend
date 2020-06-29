ea6decc087078cf5ad18ea33a95c03f044b61c0d
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-29 23:55:30 +0100

    Repo typeOrm

--- src/database/migrations/1593434644634-CreateAppointments.ts
+++ src/database/migrations/1593434644634-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1593434644634
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',

5d23a33a910f39c581a9234e7108326abc638e26
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-30 10:04:19 +0100

    fixed model Appointment

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('appointmets')
+@Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;

3ce33b5589b7cd01c55febbc8b7167cdd0ff3ccc
Marcos Lucas <marcosvaldeni@gmail.com>
2023-02-18 21:59:52 +0000

    rewriting repositories

--- src/modules/appointments/service/CreateAppointmentService.ts
+++ src/modules/appointments/service/CreateAppointmentService.ts
@@ -2,8 +2,8 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
 import AppError from '../../../shared/errors/AppError';
+import AppointmentsRepository from '../infra/repositories/AppointmentRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentRepository';
 
 interface Request {
   provider_id: string;
@@ -29,8 +29,6 @@ class CreateAppointmentService {
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
